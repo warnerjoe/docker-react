5a443dd0b2e880e35fb01f2f2268c505e7d422c7
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2025-01-25 18:23:55 -0600

    Changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Can I just change this text?
         </p>
         <a
           className="App-link"
